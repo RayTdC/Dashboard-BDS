fee3c597a719fcddc01864cb966269c909509c0f
Kesia <112557978+kesia8@users.noreply.github.com>
2023-01-13 11:55:22 -0300

    Adicionando comandos de criação do banco de dados

---
+++ script-sql.sql
@@ -0,0 +1,17 @@
+ create database steam;
+ 
+ 
+ CREATE TABLE `jogo` (
+  `id` int(11) DEFAULT NULL,
+  `nome` text DEFAULT NULL,
+  `data_de_lançamento` text DEFAULT NULL,
+  `plataforma` text DEFAULT NULL,
+  `genêros` text DEFAULT NULL,
+  `avaliações_positivas` int(11) DEFAULT NULL,
+  `avaliações_negativas` int(11) DEFAULT NULL,
+  `media_tempo_jogado` int(11) DEFAULT NULL,
+  `preço` double DEFAULT NULL
+);
+alter 
+
+
