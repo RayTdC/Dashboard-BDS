d7a09850fc195a2049ea53cc45f0d0b99b136fb8
Raísha Tinoco <112362301+RayTdC@users.noreply.github.com>
2023-01-15 20:55:20 -0300

    Add files via upload

---
+++ queries.sql/raisha.sql
@@ -0,0 +1,8 @@
+
+/*1- Quais os primeiros jogos lançados na plataforma?*/
+
+SELECT * FROM steam_main ORDER BY data_de_lancamento ASC LIMIT 5;
+
+/*2- Quais os jogos mais bem avaliados da Steam ?*/
+
+SELECT * FROM steam_main ORDER BY avaliacoes_positivas DESC LIMIT 10;
