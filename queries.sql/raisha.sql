b5f589be7db813208cb81de14a765611ad607f3e
Raísha Tinoco <112362301+RayTdC@users.noreply.github.com>
2023-01-15 20:56:17 -0300

    Update raisha.sql

--- queries.sql/raisha.sql
+++ queries.sql/raisha.sql
@@ -6,3 +6,4 @@ SELECT * FROM steam_main ORDER BY data_de_lancamento ASC LIMIT 5;
 /*2- Quais os jogos mais bem avaliados da Steam ?*/
 
 SELECT * FROM steam_main ORDER BY avaliacoes_positivas DESC LIMIT 10;
+
